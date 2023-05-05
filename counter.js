963e69417b8c42e26ab7fa62ac5a8f4349e23b72
richardcsedge <132105798+richardcsedge@users.noreply.github.com>
2023-05-05 16:51:22 -0400

    1 file modified

--- counter.js
+++ counter.js
@@ -4,11 +4,35 @@ const countSpan = document.getElementById('count');
 const incrementButton = document.getElementById('increment');
 const decrementButton = document.getElementById('decrement');
 
+let count = 10;
+countSpan.innerText = count;
+
+const updateUi = () => {
+  console.log({ count });
+  countSpan.innerText = count;
+};
+
+incrementButton.addEventListener('click', () => {
+  count++;
+  updateUi();
+  console.log('increment button clicked');
+});
+
+decrementButton.addEventListener('click', () => {
+  count--;
+  updateUi();
+  console.log('decrement button clicked');
+});
+
+/** I have found this code also works:
+ * 
 incrementButton.addEventListener('click', () => {
   countSpan.innerText++;
   console.log('increment button clicked');
 });
+
 decrementButton.addEventListener('click', () => {
   countSpan.innerText--;
   console.log('decrement button clicked');
 });
+ */
